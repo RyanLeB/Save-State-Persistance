177d0c5a7cd3f764ae2b3e164e26cb56a2b675b1
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-09-18 10:38:09 -0300

    scene loader

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,10 +14,11 @@ public class GameManager : MonoBehaviour
 
     private int currentLevel;
     private int playerExp;
+    public SceneLoader sceneLoader;
 
-    private string saveFilePath;   
-    
-    
+
+
+
     // Start is called before the first frame update
     private void Awake()
     {
@@ -38,7 +39,28 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // press 1
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            sceneLoader.LoadScene(0);
+        }
 
+        // press 2
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            sceneLoader.LoadScene(1);
+        }
+
+        // press 3
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            sceneLoader.LoadScene(2);
+        }
+
+        // press 4
+        if (Input.GetKeyDown(KeyCode.Alpha4))        {
+            sceneLoader.LoadScene(3);
+        }
     }
 
     public void StartGame()
