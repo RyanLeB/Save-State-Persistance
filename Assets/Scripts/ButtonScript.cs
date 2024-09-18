1ea98d03b7d36f639e7c83aca2b308b181537c67
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-09-18 11:56:57 -0300

    GUI implemented

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 100, 100, 30), "Health Up"))
+        {
+            GameManager.instance.playerHealth += 10;
+        }
+        if (GUI.Button(new Rect(10, 140, 100, 30), "Health Down"))
+        {
+            GameManager.instance.playerHealth -= 10;
+        }
+        if (GUI.Button(new Rect(10, 180, 100, 30), "Exp Up"))
+        {
+            GameManager.instance.playerExp += 10;
+        }
+        if (GUI.Button(new Rect(10, 220, 100, 30), "Exp Down"))
+        {
+            GameManager.instance.playerExp -= 10;
+        }
+        if (GUI.Button(new Rect(10, 260, 100, 30), "Mana Up"))
+        {
+            GameManager.instance.playerMana += 10;
+        }
+        if (GUI.Button(new Rect(10, 300, 100, 30), "Mana Down"))
+        {
+            GameManager.instance.playerMana -= 10;
+        }
+        if (GUI.Button(new Rect(10, 340, 100, 30), "Stamina Up"))
+        {
+            GameManager.instance.playerStamina += 10;
+        }
+        if (GUI.Button(new Rect(10, 380, 100, 30), "Stamina Down"))
+        {
+            GameManager.instance.playerStamina -= 10;
+        }
+        if (GUI.Button(new Rect(10, 420, 100, 30), "Save"))
+        {
+            GameManager.instance.Save();
+        }
+        if (GUI.Button(new Rect(10, 460, 100, 30), "Load"))
+        {
+            GameManager.instance.Load();
+        }
+    }
+    
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -8,12 +9,15 @@ using UnityEngine.Playables;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance { get; private set; }
    
     
-    public static GameManager Instance { get; private set; }
 
-    private int currentLevel;
-    private int playerExp;
+    public int currentLevel;
+    public float playerExp;
+    public float playerHealth;
+    public float playerMana;
+    public float playerStamina;
     public SceneLoader sceneLoader;
 
 
@@ -22,9 +26,9 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     private void Awake()
     {
-        if (Instance == null)
+        if (instance == null)
         {
-            Instance = this;
+            instance = this;
             DontDestroyOnLoad(gameObject);
             
         }
@@ -63,21 +67,62 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void StartGame()
+
+    private void OnGUI()
     {
-        currentLevel = 1;
-        playerExp = 0;
-
-
+        GUI.Label(new Rect(10, 10, 100, 20), "Level: " + currentLevel);
+        GUI.Label(new Rect(10, 30, 100, 20), "Exp: " + playerExp);
+        GUI.Label(new Rect(10, 50, 100, 20), "Health: " + playerHealth);
+        GUI.Label(new Rect(10, 70, 100, 20), "Mana: " + playerMana);
+        GUI.Label(new Rect(10, 90, 100, 20), "Stamina: " + playerStamina);
     }
 
 
-    public void LoadGame()
+    // Saves the game data
+    public void Save()
     {
-   
-        
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/gameData.save", FileMode.Open);
+
+        PlayerData data = new PlayerData();
+        data.currentLevel = currentLevel;
+        data.playerExp = playerExp;
+        data.playerHealth = playerHealth;
+        data.playerMana = playerMana;
+        data.playerStamina = playerStamina;
+
+        bf.Serialize(file, data);
+        file.Close();
     }
 
-   
+    // Loads the game data
+    public void Load()
+    {
+        if(File.Exists(Application.persistentDataPath + "/gameData.save"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gameData.save", FileMode.Open);
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+
+            currentLevel = data.currentLevel;
+            playerExp = data.playerExp;
+            playerHealth = data.playerHealth;
+            playerMana = data.playerMana;
+            playerStamina = data.playerStamina;
+        }   
+    }
 
 }
+
+// Storage class for player data
+[System.Serializable]
+class PlayerData
+{
+    public int currentLevel;
+    public float playerExp;
+    public float playerHealth;
+    public float playerMana;
+    public float playerStamina;
+}
+
